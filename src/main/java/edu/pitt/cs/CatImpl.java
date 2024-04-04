064f64ec13b0447f6aaee962a0a05ddcf8c95076
Erasto Omolo <eoo5@pitt.edu>
2024-04-04 11:29:06 -0400

    push

--- src/main/java/edu/pitt/cs/CatImpl.java
+++ src/main/java/edu/pitt/cs/CatImpl.java
@@ -2,28 +2,28 @@ package edu.pitt.cs;
 
 public class CatImpl implements Cat {
 
-	private int id;
-	private String name;
-	private boolean rented;
+	private int _id;
+	private String _name;
+	private boolean _rented;
 
 	public CatImpl(int id, String name) {
-		this.id = id;
-		this.name = name;
-		this.rented = false;
+		this._id = id;
+		this._name = name;
+		this._rented = false;
 	}
 
 	/**
-	 * Rent cat. Simply sets the rented flag to true.
+	 * Rent cat. Simply sets the _rented flag to true.
 	 */
 	public void rentCat() {
-		this.rented = true;
+		this._rented = true;
 	}
 
 	/**
-	 * Return cat. Simply sets the rented flag to false.
+	 * Return cat. Simply sets the _rented flag to false.
 	 */
 	public void returnCat() {
-		this.rented = false;
+		this._rented = false;
 	}
 
 	/**
@@ -32,48 +32,48 @@ public class CatImpl implements Cat {
 	 * @param String name New name of the cat
 	 */
 	public void renameCat(String name) {
-		this.name = name;
+		this._name = name;
 	}
 
 	/**
-	 * Accessor for name variable. Returns the name of this cat.
+	 * Accessor for _name variable. Returns the name of this cat.
 	 * 
 	 * @return String name of cat
 	 */
 
 	public String getName() {
-		return this.name;
+		return this._name;
 	}
 
 	/**
-	 * Accessor for id variable. Returns the ID of this cat.
+	 * Accessor for _id variable. Returns the ID of this cat.
 	 * 
 	 * @return int ID of this cat
 	 */
 
 	public int getId() {
-		return this.id;
+		return this._id;
 	}
 
 	/**
-	 * Accessor for rented variable. Returns if cat is rented.
+	 * Accessor for _rented variable. Returns if cat is rented.
 	 * 
 	 * @return boolean - true if rented, false otherwise
 	 */
 
 	public boolean getRented() {
-		return this.rented;
+		return this._rented;
 	}
 
 	/**
-	 * Returns string version of this cat, in form: "ID *idnum*. *name*" Example
+	 * Returns string version of this cat, in form: "ID *id_num*. *name*" Example
 	 * for cat of ID 1, name Jennyanydots: "ID 1. Jennyanydots"
 	 * 
 	 * @return String string version of cat
 	 */
 
 	public String toString() {
-		String catString = "ID " + this.id + ". " + this.name;
+		String catString = "ID " + this._id + ". " + this._name;
 		return catString;
 	}
 
